2197e3453b57b84518c0ee105698730d9a69103a
PranciskusMaceviciusAppliedInformatics <166858989+PranciskusMaceviciusAppliedInformatics@users.noreply.github.com>
2024-05-05 21:36:47 +0300

    Update convert_n-gram_ser_to_lstm_ser.py

--- convert_n-gram_ser_to_lstm_ser.py
+++ convert_n-gram_ser_to_lstm_ser.py
@@ -4,8 +4,6 @@ def merge_files(file1_path, file2_path, output_path):
             line1 = line1.split('\t')[-1].strip() + '\t'
             line2 = line2.split('\t')[-1].strip()
             output.write(line1 + line2 + '\n')
-
-# Example usage
 file1_path = 'lit_test.tsv'
 file2_path = '3lit.txt'
 output_path = 'neural_like_3.txt'
